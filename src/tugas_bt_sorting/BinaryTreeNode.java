01aeac61c87f8f0103d01aa7532d0e161c8c281c
game <nisrinaarintia@gmail.com>
2017-12-13 19:21:49 +0700

    Nisrina AM/201610370311222/3D

---
+++ src/tugas_bt_sorting/BinaryTreeNode.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_bt_sorting;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class BinaryTreeNode {
+    Object element;
+    BinaryTreeNode leftChild;
+    BinaryTreeNode rightChild;
+    public BinaryTreeNode(){}
+    public BinaryTreeNode(Object theElement){
+        element = theElement;
+    }
+    public BinaryTreeNode(Object theElement,BinaryTreeNode theleftChild,BinaryTreeNode therightChild){
+        element = theElement;
+        leftChild = theleftChild;
+        rightChild = therightChild;
+    }
+    
+}
+
